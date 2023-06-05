36fef99e8504adb4a502f285db9085d31357b119
Ryan Conrad <camalot@gmail.com>
2023-06-04 19:13:48 -0500

    fix regex

--- bot/cogs/food_photo.py
+++ bot/cogs/food_photo.py
@@ -63,7 +63,7 @@ class FoodPhoto(commands.Cog):
                 return
 
             # check if message has a link to the image in it
-            media_regex = r"(https:\/\/)?((?:cdn|media)\.discordapp\.(?:net|com))\/attachments\/\d+\/\d+\/\w+\.(png|jpg|jpeg|gif|webp))"
+            media_regex = r"(https:\/\/)?((?:cdn|media)\.discordapp\.(?:net|com))\/attachments\/\d+\/\d+\/\w+\.(png|jpg|jpeg|gif|webp)"
             # if the message is not a photo, ignore
             if not message.attachments and not re.match(media_regex, message.content):
                 self.log.debug(guild_id, "food_photo.on_message", f"Message {message.id} does not contain a photo")
