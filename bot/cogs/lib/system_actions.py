5aaeb21924bb7ff744f841874b23a498a891feea
Ryan Conrad <camalot@gmail.com>
2023-06-14 16:46:44 -0500

    remove action

--- bot/cogs/lib/system_actions.py
+++ bot/cogs/lib/system_actions.py
@@ -1,6 +1,5 @@
 from enum import Enum
 class SystemActions(Enum):
     NEW_ACCOUNT_KICK = 1
-    TWITCH_ACCOUNT_LINK = 2
-    JOIN_WHITELIST_ADD = 3
+    JOIN_WHITELIST_ADD = 2
     JOIN_WHITELIST_REMOVE = 4
