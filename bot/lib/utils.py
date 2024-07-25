f689114635acb1c5e7ac60bd00d51f9820019cdd
Ryan Conrad <camalot@gmail.com>
2024-07-25 12:57:10 -0500

    fix lint errors

--- bot/lib/utils.py
+++ bot/lib/utils.py
@@ -95,9 +95,9 @@ def from_timestamp(timestamp: float) -> datetime.datetime:
 def get_timestamp() -> float:
     return to_timestamp(datetime.datetime.now(datetime.UTC))
 
+
 def get_seconds_until(end_ts: float) -> int:
     now = get_timestamp()
-    
     return int(end_ts - now) if end_ts > now else 0
 
 
